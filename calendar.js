6edbeb6c70592e2682796097fbf672f551f4d17d
Ryan <rrawitscher101@gmail.com>
2019-02-20 15:34:06 -0600

    buncha stuff

--- calendar.js
+++ calendar.js
@@ -11,18 +11,12 @@
 function toggleForm(){
 if (document.getElementById("myForm").style.display=="block"){
   document.getElementById("myForm").style.display = "none";
+  
 }
   else {
     document.getElementById("myForm").style.display = "block";
   }
 }
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
-
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
 
 init();

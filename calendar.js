29dad166256e73446a8bad4c78560f75c889ba44
garrett <garrettmatsuda2019@u.northwestern.edu>
2019-02-20 13:32:56 -0600

    changing form

--- calendar.js
+++ calendar.js
@@ -1,5 +1,12 @@
 // Create your global variables below:
 
+// src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.datepicker');
+//     var instances = M.Datepicker.init(elems, options);
+// }
+
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }

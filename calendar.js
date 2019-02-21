2ecb83d02840517c4713a7a88d2226baf8322c9f
Ryan <rrawitscher101@gmail.com>
2019-02-20 20:37:16 -0600

    working calendar

--- calendar.js
+++ calendar.js
@@ -7,6 +7,46 @@
 //     var instances = M.Datepicker.init(elems, options);
 // }
 
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+function submitForm(){
+  if(getQueryVariable("title")){
+    var eventTitle = getQueryVariable("title");
+    var notes = getQueryVariable("notes");
+    var start = getQueryVariable("start");
+    var end = getQueryVariable("end");
+    var invite = getQueryVariable("invite");
+    var day = getQueryVariable("day");
+    console.log(eventTitle+" " +notes+" "+start+" "+end+" "+invite+ " "+ day);
+    day=day[8]+day[9]
+    starthour=start[0]+start[1];
+    endhour=end[0]+end[1];
+    diff=endhour-starthour
+    string=starthour+"-"+day;
+    var i;
+  for (i = 0; i <= diff; i++) {
+    string=(parseInt(starthour)+i)+"-"+day;
+    console.log(string)
+    document.getElementById(string).style.backgroundColor='#585858';
+    document.getElementById(string).style.color='white';
+    document.getElementById(string).innerHTML=eventTitle;
+  }
+    document.getElementById(string).style.backgroundColor='#585858';
+    document.getElementById(string).style.color='white';
+    document.getElementById(string).innerHTML=eventTitle;
+}
+}
+
+
 
 function toggleForm(){
 if (document.getElementById("myForm").style.display=="block"){

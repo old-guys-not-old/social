d175bd249818066913676cd77bbdd2416b33e785
garrett <garrettmatsuda2019@u.northwestern.edu>
2019-02-27 22:11:26 -0600

    graphics uniform

---
+++ chat.js
@@ -0,0 +1,9 @@
+
+function messageSubmitted()
+{
+	console.log("fuck!");
+    var message = document.getElementById('message-input').value;
+
+    var chatBox = document.getElementById("chat-box");
+    chatBox.appendChild("<p>hey</p>");
+}

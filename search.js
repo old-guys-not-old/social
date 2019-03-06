1a61b62044ea78e3cc1c7a14756263c58265d597
candivo <candivitoglu2015@u.northwestern.edu>
2019-03-06 11:59:50 -0600

    searchPage
    search page is added needs js implementation

---
+++ search.js
@@ -0,0 +1,27 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+function nameList() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('name-input');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("name-list");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}

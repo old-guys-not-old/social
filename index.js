55e96bf0283b6c898233a2754289edab7ac5fdda
Ryan <rrawitscher101@gmail.com>
2019-03-11 04:10:30 -0500

    start on homescreen

--- index.js
+++ index.js
@@ -12,16 +12,34 @@ function getQueryVariable(variable)
 
 function populate()
 {
-  var message = getQueryVariable("chat");
-	if(message){
-    var message = message.split("+").join(" ")
-		document.getElementById("chat-box").innerHTML='<h5 id="message">Abizar: '+ message+ '</h5>';
-	}
+  var events=$.parseJSON(sessionStorage.getItem('events')).events
+  if (events){
+    for(var p=0; p<events.length;p++){
+      var title =events[p].title;
+      var notes = events[p].notes;
+      var start = events[p].start;
+      var end = events[p].end;
+      var invite = events[p].invite;
+      var day = events[p].day;
+      day=day[8]+day[9]
+      starthour=start[0]+start[1];
+      endhour=end[0]+end[1];
+      
+    }
+  }
 }
 
+//<div class='preview-chat-box'>
+//    <p id="preview-details" class="event-name"> 6x6 Futsal game</p>
+//    <div id="edit-button" class="edit-button">
+//      <span class="glyphicon glyphicon-pencil">
+//    </div>
+//    <div id="chat-button" class='chat-button'>
+//      <span id="edit-button"class="glyphicon glyphicon-comment">
+//    </div>
+//</div>
 
 function submitfunction(){
-  console.log("yeet")
   document.getElementById("myForm").style.display="block";
   var message = document.getElementById("message-input").value;
   document.getElementById("message-input").value="";

56052a4e546ccfe86911b6451edba5bb71d81a63
Ryan <rrawitscher101@gmail.com>
2019-03-11 14:33:30 -0500

    events on homescreen work

--- index.js
+++ index.js
@@ -24,7 +24,30 @@ function populate()
       day=day[8]+day[9]
       starthour=start[0]+start[1];
       endhour=end[0]+end[1];
-      
+      console.log(title,notes,start,end,invite,day,starthour,endhour)
+      var g=document.createElement("div");
+      g.className='preview-chat-box';
+      var z=document.createElement("span")
+      z.className="glyphicon glyphicon-pencil";
+      var y=document.createElement("div");
+      y.id="edit-button"
+      y.className="edit-button"
+      var x = document.createElement("p")
+      x.id="preview-details"
+      x.className="event-name"
+      x.innerHTML=title+"<br>"+" "+"3/"+day+"/2019"+"<br>"+start+" "+end
+      var w=document.createElement("span")
+      w.className="glyphicon glyphicon-comment";
+      var t=document.createElement("div");
+      t.id="chat-button"
+      t.className="chat-button"
+      t.appendChild(w)
+      y.appendChild(z)
+
+      g.appendChild(x)
+      g.appendChild(y)
+      g.appendChild(t)
+      document.getElementById("recent-activities").appendChild(g)
     }
   }
 }
